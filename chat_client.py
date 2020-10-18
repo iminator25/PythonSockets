96fbd7085c9527655788f019a272078861b649f2
Imran Adamjee <43097446+iminator25@users.noreply.github.com>
2020-10-18 13:38:21 -0500

    Added comments to the new files

--- chat_client.py
+++ chat_client.py
@@ -3,52 +3,81 @@ import select
 import errno
 import sys
 
+# setting constants here
 HEADER_LENGTH = 10
 IP = '127.0.0.1'
 PORT = 1234
 
+# allow the user to have a username
 my_username = input('Username: ')
+
+# building the socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# connecting the socket
 client_socket.connect((IP,PORT))
+
+# will allow us to handle errors
 client_socket.setblocking(False)
 
+# encode the username and username header
 username = my_username.encode('utf-8')
 username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
+
+# sending out the username to the server
 client_socket.send(username_header + username)
 
+
 while True:
+	# building the message structure
 	message = input(f"{my_username} > ")
+
+	# if the message is not empty
 	if message:
+		# encode the message
 		message = message.encode('utf-8')
+		# build the message header
 		message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
+		# send the message
 		client_socket.send(message_header + message)
 
 	try:
+		# find all the messages and receive them
 		while True:
-			# receive things
+			# read in the headers for the received message
 			username_header = client_socket.recv(HEADER_LENGTH)
+			# if we recieved no data the connection was likley closed and we can leave
 			if not len(username_header):
 				print('connection closed by the server')
 				sys.extit()
 
+			# else receive the username
 			username_length = int(username_header.decode('utf-8').strip())
 			username = client_socket.recv(username_length).decode('utf-8')
 
+			# then receive the message
 			message_header = client_socket.recv(HEADER_LENGTH)
 			message_length = int(message_header.decode('utf-8').strip())
 			message = client_socket.recv(message_length).decode('utf-8')
 
+			# nicely print the message
 			print(f"{username} > {message}")
 
 	except IOError as e:
+		# This is normal on non blocking connections - when there are no incoming data error is going to be raised
+        # Some operating systems will indicate that using AGAIN, and some using WOULDBLOCK error code
+        # We are going to check for both - if one of them - that's expected, means no incoming data, continue as normal
+        # If we got different error code - something happened
 		if e.errno != errno.EAGAIN and e.errno != errno.EWOUDBLOCK:
 			print('Reading error', str(e))
 			sys.exit()
+		# we just didnt receive anything
 		continue
 
 
 
 	except Exception as e:
+		# any other exception, we can exit 
 		print('General error', str(e))
 		sys.exit()
 	
--- chat_server.py
+++ chat_server.py
@@ -23,59 +23,87 @@ server_socket.bind((IP, PORT))
 # listening to clients 
 server_socket.listen()
 
-
+# list of conenctied sockets, good to have for OS level instructions
 sockets_list = [server_socket]
 
+# dict for coneected clients - key = socket, value = username
 clients = {}
 
 
+# function for handling incoming messages
 def receive_message(client_socket):
 	try:
+		# reads in the header which tells us how long the message is
 		message_header = client_socket.recv(HEADER_LENGTH)
 
+		# if we recieved no data, the client closed thier socket 
 		if not len(message_header):
 			return False
 
+		# else we got data and we read the lenght of the data 
+		# and we return the message 
 		message_length = int(message_header.decode('utf-8').strip())
 		return {'header': message_header, 'data': client_socket.recv(message_length)}
 
 	except:
+		# we are here if the client closes thier connection violently
 		return False
 
 
 while True:
+	 # Calls Unix select() system call or Windows select() WinSock call with three parameters:
+    #   - rlist - sockets to be monitored for incoming data
+    #   - wlist - sockets for data to be send to (checks if for example buffers are not full and socket is ready to send some data)
+    #   - xlist - sockets to be monitored for exceptions (we want to monitor all sockets for errors, so we can use rlist)
+    # Returns lists:
+    #   - reading - sockets we received some data on (that way we don't have to check sockets manually)
+    #   - writing - sockets ready for data to be send thru them
+    #   - errors  - sockets with some exceptions
+    # This is a blocking call, code execution will "wait" here and "get" notified in case any action should be taken
 	read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
 
+	# loops over read sockets 
 	for notified_socket in read_sockets:
+		# if its a server socket its a new connection and we can accept it
 		if notified_socket == server_socket:
 			client_socket, client_address = server_socket.accept()
 
+			# read in the message 
 			user = receive_message(client_socket)
 
+			# client disconected before they sent his name
 			if user is False:
 				continue
+			# upadate socket list to add the new connection
 			sockets_list.append(client_socket)
 			clients[client_socket] = user
 
 			print(f"Accepted new connection from {client_address[0]}:{client_address[1]} username:{user['data'].decode('utf-8')}")
 
+		# an existing socket is sending a message
 		else:
+			# recieve the message
 			message = receive_message(notified_socket)
 
+			# reached if the cient disconnected so we need to get rid of them
 			if message is False:
 				print(f"Closed connection from {clients[notified_socket]['data'].decode('utf-8')}")
 				sockets_list.remove(notified_socket)
 				del clients[notified_socket]
 				continue
 
+			# gets user from notified socket and we can print the data
 			user = clients[notified_socket]
 			print(f"Received message from {user['data'].decode('utf-8')}: {message['data'].decode('utf-8')}")
 
 
+			# send out message to all other people
 			for client_socket in clients:
 				if client_socket != notified_socket:
+					# send out the user data and headers as we did previously 
 					client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
 
+	# hanling possible errors. 
 	for notified_socket in exception_sockets:
 		sockets_list.remove(notified_socket)
 		del clients[notified_socket]
